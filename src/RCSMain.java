dad376615d3c3217ea9ce8492303df2b48f6bd4a
Josh Sellers <joshuasellersd@gmail.com>
2020-01-25 19:11:15 -0500

    new class

--- src/RCSMain.java
+++ src/RCSMain.java
@@ -1,6 +1,11 @@
 public class RCSMain {
-
+    /**
+     * run ranked choice simulation
+     * @param args - given as "Candidate_Name Candidate_Party" ...
+     */
     public static void main(String[] args) {
+        for (String arg : args){
 
+        }
     }
 }
