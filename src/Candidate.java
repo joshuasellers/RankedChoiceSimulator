295f8270d828ee3359950dc933ad8320855eb16c
Josh Sellers <joshuasellersd@gmail.com>
2020-01-25 19:07:02 -0500

    new class

---
+++ src/Candidate.java
@@ -0,0 +1,10 @@
+public class Candidate {
+    private String name;
+    private String party;
+
+    public Candidate(String name, String party){
+        this.name = name;
+        this.party = party;
+    }
+
+}
