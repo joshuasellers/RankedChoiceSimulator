d01f5266bd6f533004c025b780b08ec0a7485e6f
Josh Sellers <joshuasellersd@gmail.com>
2020-01-25 20:17:12 -0500

    initial setup
    just need to test than I can run simulation

--- src/RCSMain.java
+++ src/RCSMain.java
@@ -1,25 +1,64 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
 public class RCSMain {
+
+    public static ArrayList<Candidate> getCandidates(ArrayList<Candidate> c){
+        ArrayList<Candidate> candidates = new ArrayList<>();
+        ArrayList<Integer> shuffle = new ArrayList<>();
+        for (int i = 0; i < c.size(); i++){
+            shuffle.add(i);
+        }
+        Collections.shuffle(shuffle);
+        for (Integer i : shuffle){
+            candidates.add(c.get(i));
+        }
+        return candidates;
+    }
+
+    public static ArrayList<Voter> getVoters(int v, ArrayList<Candidate> candidates){
+        ArrayList<Voter> voters = new ArrayList<>();
+        for (int i = 0; i < v; i++){
+            Voter voter = new Voter(i,getCandidates(candidates));
+            voters.add(voter);
+        }
+        return voters;
+    }
+
     /**
      * run ranked choice simulation
      * @param args - given as ` "Candidate_Name Candidate_Party" "Candidate_Name Candidate_Party" ... Number_Voters
      */
     public static void main(String[] args) {
-        int voters;
+        int v = 0;
+        int total_candidates = 0;
         ArrayList<Candidate> candidates = new ArrayList<>();
+        ArrayList<Voter> voters;
         for (String arg : args){
             try {
-                voters = Integer.parseInt(arg);
+                v = Integer.parseInt(arg);
 
-                if (voters < 1) System.exit(0);
+                if (v < 1) System.exit(0);
 
             } catch(NumberFormatException e){
                 String[] candidate_fields = arg.split(" ");
                 if (candidate_fields.length != 2) System.exit(0);
-
+                total_candidates++;
                 candidates.add(new Candidate(candidate_fields[0],candidate_fields[1]));
             }
         }
+        voters = getVoters(v, candidates);
+        System.out.println("WELCOME TO THE ELECTION");
+        System.out.println("***********************");
+        System.out.println("HERE ARE THE CANDIDATES:");
+        for (Candidate c : candidates){
+            System.out.println(c.toString());
+            System.out.println("");
+        }
+        System.out.println("***********************");
+        System.out.println("THERE ARE " + v + " TOTAL VOTERS");
+        System.out.println("***********************");
+
     }
 }
--- src/Voter.java
+++ src/Voter.java
@@ -1,2 +1,16 @@
+import java.util.ArrayList;
+
 public class Voter {
+    private int id;
+    private ArrayList<Candidate> rank;
+
+    public Voter(int id, ArrayList<Candidate> rank){
+        this.id = id;
+        this.rank = rank;
+    }
+
+    @Override
+    public String toString(){
+        return "Voter #" + this.id;
+    }
 }
